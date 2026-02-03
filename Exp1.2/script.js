8b7dff571d343126eafcbfd2c3b3d85f0376b1a0
sujalcodes01 <sujalkashyap600@gmail.com>
2026-02-03 15:22:00 +0530

    Delete Exp1.2/script.js

--- Exp1.2/script.js
+++
@@ -1,6 +0,0 @@
-const text = document.getElementById("text");
-const count = document.getElementById("count");
-
-text.addEventListener("input", () => {
-  count.innerText = text.value.length + " / 150";
-});
